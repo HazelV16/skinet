61bdf5302d4ac187cd90a6d2ae2e96697f3b91a5
Hazel161 <hazelvuong@gmail.com>
2024-03-06 21:41:58 +1030

    interface and entities updated

---
+++ Core/entities/ProductBrand.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.entities
+{
+    public class ProductBrand : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
